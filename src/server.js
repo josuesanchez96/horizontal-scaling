14e183dd59b918c3bd2bbadf945a2bb7e70f035b
josuesanchez96 <josuedavid.sanchez96@gmail.com>
2025-05-25 21:26:42 -0600

    Update server.js
    Cambie hola por holaa

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const os = require('os');
 
 const server = http.createServer((req, res) => {
     console.log('Yo estoy respondiendo desde el servidor');
-    res.end(`Hola estoy desde ${os.hostname()}!`);
+    res.end(`Holaa estoy desde ${os.hostname()}!`);
 });
 
 server.listen(3000, () => {
